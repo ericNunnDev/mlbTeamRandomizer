e5992c00e0af7f924835875e861172a801b1c874
Eric Nunn <enunn@nexient.com>
2020-04-21 13:02:35 -0400

    CRA bootstrapped. Files cleaned. Half of MLB team data entered

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tim and Eric's Team Randomizer</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/data/data.js
@@ -0,0 +1,92 @@
+export default [
+    {
+        id: 1,
+        division: 'East',
+        team: 'Baltimore Orioles',
+        image: ''
+    },
+    {
+        id: 2,
+        division: 'East',
+        team: 'Boston Red Sox',
+        image: ''
+    },
+    {
+        id: 3,
+        division: 'East',
+        team: 'New York Yankees',
+        image: ''
+    },
+    {
+        id: 4,
+        division: 'East',
+        team: 'Tampa Bay Rays',
+        image: ''
+    },
+    {
+        id: 5,
+        division: 'East',
+        team: 'Toronto Blue Jays',
+        image: ''
+    },
+    {
+        id: 6,
+        division: 'Central',
+        team: 'Chicago White Sox',
+        image: ''
+    },
+    {
+        id: 7,
+        division: 'Central',
+        team: 'Cleveland Indians',
+        image: ''
+    },
+    {
+        id: 8,
+        division: 'Central',
+        team: 'Detroit Tigers',
+        image: ''
+    },
+    {
+        id: 9,
+        division: 'Central',
+        team: 'Kansas City Royals',
+        image: ''
+    },
+    {
+        id: 8,
+        division: 'Central',
+        team: 'Minnesota Twins',
+        image: ''
+    },
+    {
+        id: 10,
+        division: 'West',
+        team: 'Houston Astros',
+        image: ''
+    },
+    {
+        id: 11,
+        division: 'West',
+        team: 'Los Angeles Angels',
+        image: ''
+    },
+    {
+        id: 12,
+        division: 'West',
+        team: 'Oakland Athletics',
+        image: ''
+    },
+    {
+        id: 13,
+        division: 'West',
+        team: 'Seattle Mariners',
+        image: ''
+    },
+    {
+        id: 14,
+        division: 'West',
+        team: 'Texas Rangers',
+        image: ''
+    }
+];
